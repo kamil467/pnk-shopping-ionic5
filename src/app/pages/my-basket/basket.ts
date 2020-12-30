11da0ad96e8b24776b6992386517be7982fd8cfd
kamil <kamil467@gmail.com>
2020-12-30 17:13:49 +0000

    popup alerts and form added

--- src/app/pages/my-basket/basket.ts
+++ src/app/pages/my-basket/basket.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from "@angular/core";
+import { AngularFireAuth } from "@angular/fire/auth";
 import { AlertController, NavController, NavParams } from "@ionic/angular";
-import { Observable } from "rxjs";
+import { Observable, Subscription } from "rxjs";
+import { first } from "rxjs/operators";
 import { BasketObj, OrderItem } from "../../interfaces/basket-interface";
 import { BasketProvider } from "../../providers/basket-provider";
 
@@ -15,10 +17,14 @@ export class BasketPage   implements OnInit {
   basketItems: Observable<BasketObj>;
   storecode: string;
   basketTotalAmount: number = 0;
+  userLoggedSubscription:Subscription;
+  isLoggedIn:boolean= false;
+
 public navParams = new NavParams();
   constructor(
     public navCtrl: NavController,
-    public basketProvider: BasketProvider,public alert:AlertController
+    public basketProvider: BasketProvider,public alert:AlertController,
+   public angularFireAuth: AngularFireAuth
   ) {
    
   }
@@ -27,8 +33,10 @@ public navParams = new NavParams();
   this.basketItems  = this.basketProvider.getBasketForOrder();  // load basket Items
   this.updateTotal();
   }
-
-
+  ionViewWillEnter()
+  {
+    this.getLoggedInUser();
+  }
 
   addQuantity(item: OrderItem) {
     console.log("Item id is:" + item.productId);
@@ -78,6 +86,26 @@ updateTotal()
   this.basketProvider.getBasketForOrder().subscribe(result=>{
     this.basketTotalAmount =  this.caluclateTotalAmount(result.items);    // perform total
     });
+}
+getLoggedInUser()
+{
+ this.userLoggedSubscription = this.angularFireAuth.authState.pipe(first()).subscribe(user =>
+  {
+    if(user)
+    {
+      this.isLoggedIn = true;
+      console.log("Logged in User :"+user.phoneNumber);
+    }
+    else{
+      this.isLoggedIn = false;
+      console.log("anonymous user");
+    }
+  });
+
+}
+ngOnDestroy()
+{
+  this.userLoggedSubscription.unsubscribe();
 }
 
 }
--- src/app/pages/phone-login/phone-login.page.ts
+++ src/app/pages/phone-login/phone-login.page.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
+import { AlertController } from '@ionic/angular';
 import { FirebaseUISignInFailure, FirebaseUISignInSuccessWithAuthResult } from 'firebaseui-angular';
-import { Observable, Subscription } from 'rxjs';
-import { take } from 'rxjs/operators';
+import { Observable, of, Subscription } from 'rxjs';
+import { first, take } from 'rxjs/operators';
 import { Customer } from '../../interfaces/account-interface';
 import { AccountProvider } from '../../providers/account-provider';
 
@@ -11,60 +12,31 @@ import { AccountProvider } from '../../providers/account-provider';
   templateUrl: './phone-login.page.html',
   styleUrls: ['./phone-login.page.scss'],
 })
-export class PhoneLoginPage implements OnInit {
+export class PhoneLoginPage  {
 
   isSuccess:boolean  = false;
   customerCreationObservableSubsc:Subscription;
-  
-  constructor(public angularFireAuth: AngularFireAuth,private accountProvider:AccountProvider) { }
+  user:Observable<any>;
+  isLoggedIn:boolean = false;
+  userLoggedSubscription:Subscription;
+  customerObj :Observable<Customer>;
+  tobeUpdatedCustomerObj:Customer
 
-  ngOnInit() {
-    const phoneNumber ="+918884081427";
-
-    // avoid memory leak problem make sure subscribtion unsubscribe after it is done.
-    this.customerCreationObservableSubsc = this.accountProvider.getCustomer(phoneNumber).pipe(take(1)).subscribe(result =>
-      {
-        if(result == null) // no user data found for given phonenumber.
-        {
-          console.log("Customer not found");
-          const customer:Customer = 
-          {
-            customerId:null,
-            address:null,
-            alternativeContact:null,
-            landmark:null,
-            name:null,
-            phoneNumber:phoneNumber,
-            postCode:null
-  
-          }
-          this.accountProvider.createCustomer(customer).then((result)=>{
-            // user has been created successfully
-                  if(result.customerId != undefined)
-                  {
-                    console.log("User Id is :"+result.customerId);
-                  }
-          });
-        }
-        else{
-          console.log("user record already present");
-        }
-      });
-
-
-
-
-
-  }
+  constructor(public angularFireAuth: AngularFireAuth,
+    private accountProvider:AccountProvider,
+    private alertController:AlertController) { }
 
  ngOnDestroy()
  {
    /**It is important to unsubscribe subscribtion if not using async pipe. */
  this.customerCreationObservableSubsc.unsubscribe();
+ this.userLoggedSubscription.unsubscribe();
  }
-
-
-
+ ionViewWillEnter()
+ {
+   this.getLoggedInUser();
+ }
+ 
   successCallback(signInSuccessData: FirebaseUISignInSuccessWithAuthResult) {
   console.log("successCallback called");
   this.isSuccess = true;
@@ -75,15 +47,142 @@ export class PhoneLoginPage implements OnInit {
   // create a customer document with userId from auth obj.
   // prompt the user to fill account details such as (name, landmark, address, alternative contact no amnd pincode.)
   // if not a new user - make the form as readonly and display edit button
-  
+  this.customerCreationObservableSubsc = this.accountProvider.getCustomer(phoneNumber).pipe(take(1)).subscribe(result =>
+    {
+      if(result == null) // no user data found for given phonenumber.
+      {
+        console.log("Customer not found");
+        const customer:Customer = 
+        {
+          customerId:null,
+          address:null,
+          alternativeContact:null,
+          landmark:null,
+          name:null,
+          phoneNumber:phoneNumber,
+          postCode:null
 
+        }
+        this.accountProvider.createCustomer(customer).then((result)=>{
+          // user has been created successfully
+                if(result.customerId != undefined)
+                {
+                  console.log("User Id is :"+result.customerId);
+                  this.customerObj = of(result as Customer);
+                }
+        },
+        ).catch(
+          async error => {
+            console.error("error occurred while creating the user"+error);
+            await this.presentErrorAlert();
+          }
+        );
+      }
+      else{
+        console.log("user record already present");
+        this.customerObj = of(result);
+      }
+    },
+    async error => {
+      console.error("error occurred while sigining the user"+error);
+      await this.presentErrorAlert();
+    }
+    );
+
+    // display success message on completion of observable and display  pop for error 
+    this.getLoggedInUser();
 }
  
-errorCallback(errorData: FirebaseUISignInFailure) {
-  console.log("errorCallback called");
+  async errorCallback(errorData: FirebaseUISignInFailure) {
+  console.error("error occurred while sigining the user"+errorData);
+      await this.presentErrorAlert();
+  //display popup alert
 }
  
 uiShownCallback() {
   console.log("uiShownCallback called");
+}
+editDetails()
+{
+ // build tobeUpdatedCustomerObj.
+ 
+
+}
+
+getLoggedInUser()
+{
+ this.userLoggedSubscription = this.angularFireAuth.authState.pipe(first()).subscribe(user =>
+  {
+    if(user)
+    {
+      this.isLoggedIn = true;
+      this.customerObj = this.accountProvider.getCustomer(user.phoneNumber);
+    }
+    else{
+      this.isLoggedIn = false;
+    }
+  },
+  async error => {
+    console.error("error occurred while getting authState"+error);
+    await this.presentErrorAlert();
+  }
+  );
+
+}
+
+async logOutButtonClicked()
+{
+ await this.presentLogOutConfirm();
+}
+
+logOut()
+{
+  // show pop up alert for prompting the user to select 
+ this.angularFireAuth.signOut().then(() =>
+ {
+   console.log("successfully loggedout");
+   this.getLoggedInUser();
+ }).catch(async (err)=>
+ {
+   console.error("Error occurred while signing out the user."+err);
+   await this.presentErrorAlert();
+   // show pop alert here.
+ });
+}
+async presentLogOutConfirm() {
+  const alert = await this.alertController.create({
+    cssClass: 'my-custom-class',
+    header: 'Confirm!',
+    message: '<strong>LogOut</strong>!!!',
+    buttons: [
+      {
+        text: 'Cancel',
+        role: 'cancel',
+        cssClass: 'secondary',
+        handler: () => {
+         // do nothing.
+        }
+      }, {
+        text: 'Okay',
+        handler: () => {
+          this.logOut();
+        }
+      }
+    ]
+  });
+
+  await alert.present();
+}
+
+async presentErrorAlert() {
+  const alert = await this.alertController.create({
+    cssClass: 'my-custom-class',
+    header: 'Alert',
+    subHeader: 'Error occurred',
+    message: 'Please try again later.',
+    buttons: ['OK']
+  });
+
+  await alert.present();
 }
 }
